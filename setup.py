6a73872ae7ca40c480b180c4ff8bd17884b8fa5d
Brett Cannon <brett@python.org>
2013-09-20 19:09:02 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('docs/index.rst') as file:
 long_description = index_text.partition('.. END README')[0]
 long_description += index_text.rpartition('.. LINKS')[2]
 
-version = '33.0.0'
+version = '33.0.1'
 assert version.startswith('{}{}'.format(*sys.version_info))
 
 setup(
     name='mnfy',
     # First digit is the Massive/feature version of Python, rest are
     # feature/bugfix for mnfy.
-    version='33.0.0',
+    version=version,
     author='Brett Cannon',
     author_email='brett@python.org',
     url='http://mnfy.rtfd.org/',
