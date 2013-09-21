f158c7fb26a481e0fff35f7896f029351116d01f
Brett Cannon <brett@python.org>
2013-09-21 12:18:49 -0400

    Make tests.__main__ return a proper exit code upon failure

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,6 +1,9 @@
-import unittest
+if __name__ == '__main__':
+    import sys
+    import unittest
 
-loader = unittest.TestLoader()
-tests = loader.discover('.')
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    testRunner = unittest.runner.TextTestRunner()
+    result = testRunner.run(tests)
+    sys.exit(0 if result.wasSuccessful() else 1)
