b4b8a9c94579324150a52651b11b72d16a450874
Brett Cannon <brett@python.org>
2013-09-21 12:25:56 -0400

    Simplify testing by just relying on test discovery

---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+# Use ``python3 -m unittest discover tests`` from the top-level project
+# directory to execute all tests.
--- tests/__main__.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    import unittest
-
-    loader = unittest.TestLoader()
-    tests = loader.discover('.')
-    testRunner = unittest.runner.TextTestRunner()
-    result = testRunner.run(tests)
-    sys.exit(0 if result.wasSuccessful() else 1)
